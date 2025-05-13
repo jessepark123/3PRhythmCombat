ba21e2a4b990a81b20251b680df59ad0726a0e7a
Jesse Park <jessepark123@gmail.com>
2025-05-13 00:46:07 -0700

    Added code parts of project files

---
+++ Source/RhythmCombat/RhythmCombatGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RhythmCombatGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARhythmCombatGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARhythmCombatGameMode();
+};
+
+
+
